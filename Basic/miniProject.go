82302ca07f79881b98e0f17a97bb0859a520d874
mr9 asif <asifalibd002@gmail.com>
2025-12-14 02:47:18 +0600

    update and use package with mod

--- Basic/miniProject.go
+++ Basic/miniProject.go
@@ -1,38 +1,38 @@
-package main
+// package main
 
-import "fmt"
+// import "fmt"
 
-func welcome(){
-	fmt.Println("Welcome to our application!🙂")
-}
+// func welcome(){
+// 	fmt.Println("Welcome to our application!🙂")
+// }
 
-// get name and return name
-func name(){
-	var name string
-	fmt.Println("What's your name: ")
-	fmt.Scanln(&name)
-	fmt.Println("Ok", name , "let's move next 🙂")
-}
+// // get name and return name
+// func name(){
+// 	var name string
+// 	fmt.Println("What's your name: ")
+// 	fmt.Scanln(&name)
+// 	fmt.Println("Ok", name , "let's move next 🙂")
+// }
 
-func add(){
-	var num1, num2 int
-	fmt.Println("Give number 1: ")
-	fmt.Scanln(&num1)
-	fmt.Println("Give number 2: ")
-	fmt.Scanln(&num2)
-	fmt.Println("your num1:", num1, "num2: ", num2)
-	sum:=num1+num2
-	fmt.Println("Sum is: ", sum)
-}
+// func add(){
+// 	var num1, num2 int
+// 	fmt.Println("Give number 1: ")
+// 	fmt.Scanln(&num1)
+// 	fmt.Println("Give number 2: ")
+// 	fmt.Scanln(&num2)
+// 	fmt.Println("your num1:", num1, "num2: ", num2)
+// 	sum:=num1+num2
+// 	fmt.Println("Sum is: ", sum)
+// }
 
-func bye(){
-	fmt.Println("Thank you. Bye 🙂")
-}
+// func bye(){
+// 	fmt.Println("Thank you. Bye 🙂")
+// }
 
-func main(){
-     welcome()
-	 name()
-	 add()
- bye()
+// func main(){
+//      welcome()
+// 	 name()
+// 	 add()
+//  bye()
 
-}
+// }
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "example.com/mathlib"
+
+func main() {
+	mathlib.Add()
+}
